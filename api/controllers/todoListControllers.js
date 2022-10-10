bc9311991d8ac5adda8ce28be9e2f31086d1e6df
anila-maria <anilamariajose@gmail.com>
2022-10-09 23:43:52 -0400

    delete in route and controller

--- api/controllers/todoListControllers.js
+++ api/controllers/todoListControllers.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Courses = mongoose.model('courses');
 
 //Display list of all tasks
-exports.list_all_cousre = function(req,res){
-	Courses.find({},function(err,course){
-		if(err)
+exports.list_all_courses = function (req, res) {
+	Courses.find({}, function (err, course) {
+		if (err)
 			res.send(err);
 		res.json(course);
 	});
@@ -12,12 +12,21 @@ exports.list_all_cousre = function(req,res){
 
 
 
-exports.create_a_course = function(req,res){
+exports.create_a_course = function (req, res) {
 	var new_course = new Courses(req.body);
-	new_course.save(function(err,course){
-		if(err)
+	new_course.save(function (err, course) {
+		if (err)
 			res.send(err);
 		res.json(course);
 	});
+};
+exports.deleteCourse = async (req, res) => {
+	try {
+		const { Courseid } = req.params;
+		const courses = await Courses.deleteOne({ Courseid });
+		return response({ res, data: courses });
+	} catch (err) {
+		response({ res, err });
+	}
 };
 
--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -1,10 +1,13 @@
-module.exports = function(app){
+module.exports = function (app) {
 	var toDoList = require('../controllers/todoListControllers');
 
 
 	//todoList Routes
 
 	app.route('/courses')
-	.get(toDoList.list_all_cousre)
-	.post(toDoList.create_a_course);
+		.get(toDoList.list_all_courses)
+		.post(toDoList.create_a_course);
+
+	app.route("/deleteCourse/:Courseid")
+		.delete(toDoList.deleteCourse)
 };
